d0edb3be70d853daf88bf0e6e23abb993e78d50d
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-08 14:38:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - program using puts to display text
+*
+*Return:0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
