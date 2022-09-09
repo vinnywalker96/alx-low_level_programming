e06920872eb9632a4849794717ab693ed8f04187
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-09 15:15:32 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e')
+		{
+			continue;
+		}
+		if (alpha == 'q')
+		{
+			continue;
+		}
+		putchar(alpha);
+
+	putchar('\n');
+	}
+
+	return (0);
+
+}
