2db7ef5eb1014a7b9f324db3e285a8815910dcb6
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-09 15:58:18 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints num 0 - 9
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
