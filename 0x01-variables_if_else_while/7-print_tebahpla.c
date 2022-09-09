b28dbc626bd6c4f6b0cd76a8515cac9100f9e6f3
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-09 16:22:58 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - program that prints the lowercase alphabet in reverse
+*
+*Return:0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
