0d8dac809ac5008d814a4410a946fd1225294e82
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-16 13:01:49 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+
+}
