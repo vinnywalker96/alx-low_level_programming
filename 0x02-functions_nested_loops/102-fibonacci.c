9f8f44c776d365f8ca89e190f86b692d28ea5fff
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-16 14:19:52 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Print the 1st 50 fibonacci
+ * numbers
+ *
+ * Return (0);
+ */
+
+int main(void)
+{
+	int i;
+	long int num1, num2, sum;
+
+	for (i = 0; i < 50; i++)
+	{
+		sum = num1 + num2;
+		printf("%lu", sum);
+
+		num1 = num2;
+		num2 = sum;
+
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+
+	return (0);
+
+}
