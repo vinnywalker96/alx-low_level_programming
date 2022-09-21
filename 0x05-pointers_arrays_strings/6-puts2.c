95f53faf86954783e86775d1aeb92269eb3f9794
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-21 15:53:40 -0700

    6-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * putsi2 - prints every other character
+ * @str: string of the argument
+ */
+
+void puts2(char *str)
+{
+	int i, len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len -= 1;
+	for (i = 0; i <= len; i += 2)
+	{
+		
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+
+}
