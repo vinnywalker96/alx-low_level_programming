979af260f03b7d762cddd8c7e27af33fdabd08e5
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-22 08:30:51 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * *_strcpy - copies the string pointed to by src
+  * @dest: dereferencing var
+  * @src: pointer var
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *str = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	return (str);
+
+}
