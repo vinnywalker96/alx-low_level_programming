e6d466ca636e5f9633f661de1a1716a11bad7626
vinnywalker96 <vinnywalker96@icloud.com>
2022-09-28 15:33:50 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *_print_rev_recursion - function that print str in reverse
+  *@s: string input
+  *Return: void
+  */
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
+
