cb68f265767668895ef37907624c66d8a8651579
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-03 14:42:25 -0700

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - function that return x to the power of y
+  * @x: the first argument
+  * @y: the second argument
+  * Return : x ^ y
+  */
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
