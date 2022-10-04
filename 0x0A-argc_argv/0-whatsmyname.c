aef698990fc6372320e7bf30826f39a6fdb57d21
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-04 04:01:50 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * @argc: counts variable
+  * @argv: vector variable
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
