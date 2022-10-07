716e5cda742af5f635a7db70b8729ec10685e4a4
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-07 08:03:05 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,11 +19,6 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	if (c == 0)
-	{
-		return (NULL);
-	}
-
 	dest = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
