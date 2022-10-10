53d0f1e4fa0bda572217c3e31e648f36c9a789c1
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-10 21:35:34 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+
+
+/**
+  * struct dog - Dog Struct
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: name of owner
+  *
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
