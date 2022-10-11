32413a7ee2847fd28559588e3a3d0bd4aacfeecb
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-11 07:02:23 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,8 +19,8 @@ void print_dog(struct dog *d)
 	{
 		printf(" \n");
 	}
-	printf("Name: %s\n",d-> name);
-	printf("Age: %f\n",d-> age);
-	printf("Owner: %s\n",d-> owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 
 }
