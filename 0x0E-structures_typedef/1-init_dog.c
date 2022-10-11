11ee5a7bd00045f0c1b8eaace297bccf8469c28f
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-11 11:16:08 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
   * init_dog - function that initialize a
   *            variable of type struct dog
@@ -10,6 +11,7 @@
   * @age: age of dog
   * @owner: owner of dog
   *
+  * 
   */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		printf(" \n");
 	}
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	printf("Age: %6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 
 }
