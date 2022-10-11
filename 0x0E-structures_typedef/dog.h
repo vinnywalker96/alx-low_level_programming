9c863729f799c97510d0499691021b820c9d3473
vinnywalker96 <vinnywalker96@icloud.com>
2022-10-11 14:12:36 +0000

    Typedef struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,29 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
-  * struct dog - Dog Struct
+  * struct dog - Typedef struct of dog_t
   * @name: name of dog
   * @age: age of dog
   * @owner: name of owner
   *
+  * Description: typedef struct dog_t
   */
 
-struct dog
+
+struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+  * dog_t - Typedef of struct dog
+  */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
