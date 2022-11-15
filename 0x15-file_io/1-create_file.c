2498d0b39068a892a10a48d477e795239d8031d7
Vinny <vinnywalker96@icloud.com>
2022-11-15 00:58:12 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,33 @@
 #include "main.h"
-#include <string.h>
 
 /**
-  * create_file - creates a file
-  * @filename: filename
-  * @text_content:  content
-  * Return: new file
-  */
-
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int o, r;
-	int len = strlen(text_content);
-	char *buf;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
-	if (o == -1)
-		exit(1);
-	write(o, text_content, len);
-	if (text_content == NULL)
+
+	if (text_content != NULL)
 	{
-		return (1);
-
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	else
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
 
-	o = open(filename, O_RDONLY);
-	if (o == -1)
-		exit(1);
-	buf = text_content;
-	r = read(o, buf, len);
-	close(o);
-
-	if (!filename && !text_content)
-		return (-1);
-	else
-		return (1);
-
-
-	return (r);
+	return (1);
 }
