41758d0deef37d7c0b044ee298db8ac56ea4e93c
vinnywalker96 <vinnywalker96@icloud.com>
2022-12-13 17:01:29 +0000

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,16 +15,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		(*head) = new_node;
-		return (NULL);
-	}
-	(*head)->prev = new_node;
 	new_node->next = (*head);
+	new_node->prev = NULL;
+
+	if ((*head) != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 	(*head) = new_node;
 
 	return (new_node);
