f9db0b282fa58a1a204e8270e74b820f4cc4b37e
vinnywalker96 <vinnywalker96@icloud.com>
2023-02-03 15:27:08 +0000

    betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,6 +14,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_node_t *) * size);
+	if (table == NULL)
+		return (NULL);
 
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
