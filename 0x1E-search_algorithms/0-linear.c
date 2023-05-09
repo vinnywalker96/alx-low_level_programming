41e2ba933d526cefd6ab5f32a05d073c9c806d06
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-09 04:55:50 -0700

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,19 +6,22 @@
  * @size: size of the array
  * @value: value to search for
  *
- * Return : Index of value
+ * Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
+
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i <= (int)size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
 			return (i);
-		}
+
 	}
 	return (-1);
 }
