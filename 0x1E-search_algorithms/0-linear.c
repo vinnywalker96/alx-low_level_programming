af2edad8f0fbec9b7aa413ed944dd24fde2eb79b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-09 04:25:06 -0700

    0-linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: The array of integers
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return : Index of value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	for (i = 0; i <= size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS */
