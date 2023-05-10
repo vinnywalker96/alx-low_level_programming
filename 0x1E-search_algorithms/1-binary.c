1e44d71bbbdbb642a3cd68fd399dd3ff622392ca
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-10 12:20:38 +0000

    1-binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left;
+	int right;
+	int mid;
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	left = 0;
+	right = size - 1;
+
+
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Search in array: ");
+
+		for (i = left; i <= right; i++)
+		{
+			printf("%d ", array[i]);
+		}
+
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+
+		else if (array[mid] < value)
+			left = mid + 1;
+
+		else
+			right = mid - 1;
+
+	}
+
+	return (-1);
+}
