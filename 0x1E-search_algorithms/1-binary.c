1748acb955694ee4f44c7c4a0926e5c079725796
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-11 14:56:41 +0000

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,9 +11,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int left;
-	int right;
-	int i;
+	int left, right, i;
 
 	if (array == NULL)
 	{
@@ -25,24 +23,27 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		int mid = left + (right - left) / 2;
+
 		printf("Search in array: ");
 
 		for (i = left; i <= right; i++)
 		{
-			printf("%d ", array[i]);
-		}
+			printf("%d", array[i]);
+			if (i != right)
+			{
+				printf(",");
+				printf(" ");
+			}
 
+		}
 		printf("\n");
 
 		if (array[mid] == value)
 			return (mid);
-
 		else if (array[mid] < value)
 			left = mid + 1;
-
 		else
 			right = mid - 1;
-
 	}
 
 	return (-1);
